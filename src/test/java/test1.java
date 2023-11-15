c4d1d6d4ff21b39cb7229a7f4f5ebf21ca8326f3
Nikhil Abburi <nikhil_abburi@epam.com>
2023-11-15 18:27:08 +0530

    updated

--- src/test/java/test1.java
+++ src/test/java/test1.java
@@ -1,11 +1,22 @@
+import com.aventstack.extentreports.ExtentReporter;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import org.testng.annotations.Test;
 
 public class test1 {
 
 
-
     @Test
     public static void printHello(){
+        ExtentHtmlReporter htmlreporter =new ExtentHtmlReporter("testngreport.html");
+        ExtentReports report =new ExtentReports();
+        report.attachReporter(htmlreporter);
+        ExtentTest test= report.createTest("this ia test","just a dummy report");
+        test.log(Status.INFO,"nice duty");
         System.out.print("hello world");
+        test.pass("this is passed and printed hello world");
+        report.flush();
     }
 }
